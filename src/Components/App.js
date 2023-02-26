02e061983735e19ee3f3d1e6aaf0137d41c03a01
Kai Mm <62195892+KaiMilo@users.noreply.github.com>
2023-02-26 16:47:07 -0600

    Changes
    New changes

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,39 +4,37 @@ import Filter from './Filter'
 import PersonsFiltered from './PersonsFiltered'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-      { id: 1,
-        name: 'Arto Hellas',
-        number: '232323232323'},
-        { id: 2,
-          name: 'Tomias Evans',
-          number: '232323232323'}
-    ])
-    const [newName, setNewName]= useState({name: '', number: ''})
-    const [newFilter, setNewFilter] = useState('')
+  const [persons, setPersons] = useState([
+    { id: 1, name: 'Arto Hellas', number: '040-123456'},
+    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523'},
+    { id: 3, name: 'Dan Abramov', number: '12-43-23435'},
+    { id: 4, name: 'Mary Poppendieck', number: '39-23-64-6423122'}
+  ])
+  const [newName, setNewName]= useState({name: '', number: ''})
+  const [newFilter, setNewFilter] = useState('')
+  const repeated = persons.some((element) => element.name.toUpperCase() === newName.name.toUpperCase()) 
+  //Add person function
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log(event.target)
+    console.log(repeated)
+    const PersonObject = {
+      id: persons.length + 1,
+      name: newName.name ,
+      number: newName.number}
+    newName.name === '' || newName.number === '' ? alert('Both values are nedeed') :
+    repeated ? alert(`${newName.name} is already added to the phonebook`) : 
+    setPersons(persons.concat(PersonObject))
+    setNewName({name: '', number: ''}) 
+  }
 
-    const addPerson = (event) => {
-        event.preventDefault()
-        console.log(event.target)
-        const repeated = persons.some((element) => element.name.toUpperCase() === newName.name.toUpperCase()) 
-        console.log(repeated)
-        const PersonObject = {
-          id: persons.length + 1,
-          name: newName.name ,
-          number: newName.number}
-        repeated ? alert(`${newName.name} is already added to the phonebook`) : 
-          setPersons(persons.concat(PersonObject))
-          setNewName({name: '', number: ''}) 
-        
-        
-    }
-    const handleNotChangeName = (event) => {
-        console.log(event.target)
-        setNewName({...newName, name: event.target.value})
-    }
-    const handleNotChangeNumber = (event) => {
-      console.log(event.target)
-      setNewName({...newName, number: event.target.value})
+  const handleNotChangeName = (event) => {
+    console.log(event.target)
+    setNewName({...newName, name: event.target.value})
+  }
+  const handleNotChangeNumber = (event) => {
+    console.log(event.target)
+    setNewName({...newName, number: event.target.value})
   } 
   const handleFilter = (event) => {
     console.log(event.target)
@@ -44,26 +42,26 @@ const App = () => {
   }
 
   const personsFiltered = 
-  newFilter === '' ? persons : 
-  persons.filter(
-    (x) => x.name
-    .toLocaleLowerCase()
-    .indexOf(newFilter.toLocaleLowerCase())> -1)
+    newFilter === '' ? persons : 
+    persons.filter(
+      (x) => x.name
+        .toLocaleUpperCase()
+        .indexOf(newFilter.toLocaleUpperCase())>-1)
 
-    return (
-      <div>
-        <h2> PhoneBook</h2>
-        <Filter handleFilter={handleFilter}/>
-        <h2>add a New one</h2>
-        <PersonForm
+  return (
+    <div>
+      <h2> PhoneBook</h2>
+      <Filter handleFilter={handleFilter}/>
+      <h2>add a New one</h2>
+      <PersonForm
         addPerson={addPerson}
         newName={newName}
         handleNotChangeName={handleNotChangeName}
         handleNotChangeNumber={handleNotChangeNumber}/>
-        <h2>Numbers</h2>
-            <PersonsFiltered personsFiltered={personsFiltered}/>
-      </div>
-    )
-  }
+      <h2>Numbers</h2>
+      <PersonsFiltered personsFiltered={personsFiltered}/>
+    </div>
+  )
+}
 
 export default App
