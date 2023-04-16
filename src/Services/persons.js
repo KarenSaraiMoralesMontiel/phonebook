84a8e1ed01e1722dd79d9034c93a6be090334237
Kai Mm <karen.sarai.morales@gmail.com>
2023-04-15 23:33:26 -0600

    Update persons.js

--- src/Services/persons.js
+++ src/Services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url=`/api/persons`
+const url=`https://phonebook-server-private.onrender.com/api/persons`
 
 const getAll = () => {
     const request = axios.get(url) 
